d3eae75d561f424295f35cb37d0e58972618c825
= <=>
2020-06-21 09:22:52 +0100

    Web admin and json requests

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <FastLED.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266mDNS.h>
 #include <WiFiManager.h> 
+#include <ArduinoJson.h>
 
 // -----------------------------------------------------------------------------
 
@@ -20,13 +23,18 @@ char light_name[40];
 CRGB leds[NUM_LEDS];
 
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-WiFiServer server(80);
+ESP8266WebServer server(80);
 String header;
-String outputState = "off";
+boolean outputState = false;
 
 
 const unsigned long CONNECT_TIMEOUT = 10; // Wait 10 Seconds  to connect to the real AP before trying to boot the local AP
 const unsigned long AP_TIMEOUT = 20; // Wait 20 Seconds in the config portal before trying again the original WiFi creds
+
+void handleRoot();
+void updateLight();
+void getLight();
+void handleNotFound();
 
 void setup() {
     Serial.begin(SERIAL_BAUDRATE);
@@ -50,9 +58,56 @@ void setup() {
 
     FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.setBrightness(BRIGHTNESS);
+    if (!MDNS.begin("lamp")) {             // Start the mDNS responder for esp8266.local
+      Serial.println("Error setting up MDNS responder!");
+    }
+    Serial.println("mDNS responder started");
+
+    server.on("/", HTTP_GET, handleRoot);
+    server.on("/led", HTTP_GET, getLight);
+    server.on("/led", HTTP_POST, updateLight);
+    server.onNotFound(handleNotFound);
+    server.begin();
 
 }
 
+void handleRoot() {
+
+  HTTPClient http;  //Declare an object of class HTTPClient
+  String payload;
+
+  http.begin("http://192.168.1.85:5000/");  //Specify request destination
+  int httpCode = http.GET();                                                                  //Send the request
+ 
+  if (httpCode > 0) { //Check the returning code
+    payload = http.getString();   //Get the request response payload
+    Serial.println(payload);                     //Print the response payload
+    
+  }
+  
+  http.end();  
+  server.send(200, "text/html", payload);
+}
+
+void updateLight() {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, server.arg("plain"));
+  outputState = doc["status"];
+  Serial.println(server.arg("plain"));
+  server.send ( 200, "text/json", "{success:true}" );
+}
+
+void getLight(){
+  if (outputState) {
+    server.send ( 200, "text/json", "{status:true}" );
+  } else {
+     server.send ( 200, "text/json", "{status:false}" );
+  }
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
 
 void rainbow() 
 {
@@ -71,7 +126,7 @@ void sinelon()
 
 void loop() {
 
-
+/*
  WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
@@ -96,10 +151,10 @@ void loop() {
             // turns the GPIOs on and off
             if (header.indexOf("GET /output/on") >= 0) {
               Serial.println("Output on");
-              outputState = "on";
+              outputState = true;
             } else if (header.indexOf("GET /output/off") >= 0) {
               Serial.println("Output off");
-              outputState = "off";
+              outputState = false;
             }
             
             // Display the HTML web page
@@ -117,9 +172,9 @@ void loop() {
             client.println("<body><h1>ESP8266 Web Server</h1>");
             
             // Display current state, and ON/OFF buttons for the defined GPIO  
-            client.println("<p>Output - State " + outputState + "</p>");
+           // client.println("<p>Output - State " + outputState + "</p>");
             // If the outputState is off, it displays the ON button       
-            if (outputState=="off") {
+            if (!outputState) {
               client.println("<p><a href=\"/output/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/output/off\"><button class=\"button button2\">OFF</button></a></p>");
@@ -145,9 +200,14 @@ void loop() {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
-
-    //fill_rainbow( leds, NUM_LEDS, gHue, 7);                  
-    sinelon();
+ */
+    //fill_rainbow( leds, NUM_LEDS, gHue, 7);  
+    server.handleClient();                 
+    if(!outputState){
+      fill_solid( leds, NUM_LEDS, CRGB(0,0,0));
+    } else {
+      rainbow();
+    }
     FastLED.show();  
     FastLED.delay(1000/FRAMES_PER_SECOND); 
     EVERY_N_MILLISECONDS( 20 ) { gHue++; }
